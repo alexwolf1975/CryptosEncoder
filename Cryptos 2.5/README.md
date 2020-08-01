208ea414b0befe859f419127f0f87013d4d03417
alexwolf1975 <alex-wolf-75@mail.ru>
2020-08-02 00:56:15 +0300

    New release

---
+++ Cryptos 2.5/README.md
@@ -0,0 +1,112 @@
+# Cryptos Encoder 2.4
+
+Encryption and decryption of files using bit noise superposition and permutation with division.
+
+## Usage.
+
+`python3 cryptos2.4.py [-h] [-v] (-e | -d) (-mrs | -cnk) (-dig | -txt | -bin) -c cipher_file (-b16 | -b64 | -b256) -i input_file -o output_file`
+
+The digital cipher file must contain one line of cipher in decimal, hexadecimal, octal or binary. For example.
+
+`1234567890123456789012345678901234567890`
+
+`0x1234567890ABCDEF01234567890ABCDEF`
+
+`0X1234567890ABCDEF01234567890ABCDEF`
+
+`0o123456701234567012345670123456701`
+
+`0O123456701234567012345670123456701`
+
+`0b101010101010101010101010101010101`
+
+`0B101010101010101010101010101010101`
+
+It is possible use multiline digital cipher notation. For example.
+
+```
+12345678
+90123456
+78901234
+56789012
+34567890
+```
+```
+0x1234567890ABCD
+EF01234567890ABC
+DEF
+```
+```
+0X1234567890ABCD
+EF01234567890ABC
+DEF
+```
+
+The text cipher file must contain any text. For example.
+
+`This is a chifer`
+
+Of course, it is possible use of multiline text.
+
+```
+This is a chifer,
+very long cipher
+text.
+```
+## Options
+
+**-h, --help:** show help message and exit.  
+**-v, --verification:** print more information.  
+**-e, --encryption:** encryption action.  
+**-d, --decryption:** decryption action.  
+**-dig, --digital:** digital key.  
+**-txt, --text:** text key.  
+**-bin, --binary:** binary key. It is recommended using of files up to 10 kilobytes.  
+**-c file, --cipher file:** file with cipher key.  
+**-b16, --io_base16:** base16 input or output file. It it recommended for key generation option.  
+**-b64, --io_base64:** base64 input or output file.  
+**-b256, --io_base256:** binary input or output file.  
+**-i file, --input file:** input file.  
+**-o file, --output file:** output file.  
+
+## Examples
+
+Encryption from source to encrypted output file.
+
+`python3 cryptos2.5.py [-v] -e -dig -c cipher_file -b16 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -e -dig -c cipher_file -b64 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -e -dig -c cipher_file -b256 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -e -txt -c cipher_file -b16 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -e -txt -c cipher_file -b64 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -e -txt -c cipher_file -b256 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -e -bin -c cipher_file -b16 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -e -bin -c cipher_file -b64 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -e -bin -c cipher_file -b256 -i input_file -o output_file`
+
+Decryption from encrypted input file to source output file.
+
+`python3 cryptos2.5.py [-v] -d -dig -c cipher_file -b16 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -d -dig -c cipher_file -b64 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -d -dig -c cipher_file -b256 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -d -txt -c cipher_file -b16 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -d -txt -c cipher_file -b64 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -d -txt -c cipher_file -b256 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -d -bin -c cipher_file -b16 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -d -bin -c cipher_file -b64 -i input_file -o output_file`
+
+`python3 cryptos2.5.py [-v] -d -bin -c cipher_file -b256 -i input_file -o output_file`
---
+++ Cryptos 2.5/cryptos2.5.py
@@ -0,0 +1,230 @@
+from argparse import ArgumentParser
+from base64 import b16decode, b16encode, b64decode, b64encode
+from functools import reduce
+from secrets import token_bytes
+
+
+def bits_few(key):
+    zeros = 0
+    ones = 0
+    while (key):
+        if key & 1:
+            ones += 1
+        else:
+            zeros += 1
+
+        key >>= 1
+
+    count = zeros + ones
+    few_zeros = (100 * zeros // count < 30)
+    few_ones = (100 * ones // count < 30)
+    if few_zeros:
+        print('It is too few zeros in the encryption key!',
+              'Please try change chipher key by adding more zeros.')
+
+    if few_ones:
+        print('It is too few ones in the encryption key!',
+              'Please try change chipher key by adding more non zero bytes.')
+
+    if args.verification:
+        print(f'Chipher kes {count} lehgth consists of {zeros} zeros and {ones} ones.')
+
+    return any([few_zeros, few_ones])
+
+
+def get_salt(s):
+    sum_s = sum(s)
+    salt = (((len(s) << sum_s.bit_length()) + sum_s) << 8) + reduce(lambda x, y: x ^ y, s)
+    mul_s = reduce(lambda x, y: (x * y) % ch, filter(lambda x: x > 1, s))
+
+    return (salt << mul_s.bit_length()) + mul_s
+
+
+def int_sqrt(x):
+    z, zp = 2, 4
+    while x - z * z > zp - x:
+        z, zp = zp, zp * zp
+    zn = lambda y: (y + x // y) // 2
+    z1 = zn(z)
+    z2 = zn(z1)
+    while z1 != z and z2 != z:
+        z, z1, z2 = z1, z2, zn(z2)
+        z = min([z1, z2])
+        z1, z2 = z, z + 1
+    if abs(x - z1 * z1) < abs(x - z2 * z2):
+        return z1
+    else:
+        return z2
+
+
+def noise(s, salt=None):
+    if salt is None:
+        z = len(s)
+    else:
+        z = salt
+
+    if z > 1:
+        while z < ch:
+            z *= z
+    else:
+        z = ch % 256
+
+    i = j = k = 0
+    sl = len(s)
+    while k < sl:
+        z %= ch
+        xbl = z.bit_length() // 8
+        xb = z.to_bytes(xbl + 1, 'little')[:-1]
+        j = min(sl - k, xbl)
+        for i in range(0, j):
+            s[k] ^= xb[i]
+            k += 1
+
+        if z == 0:
+            z = ch * ch + 1
+        else:
+            z *= z ^ (ch >> (ch.bit_length() - z.bit_length()))
+
+
+def shuffle(s, sc=None):
+    if len(s) < 2:
+        return
+
+    if sc is None:
+        sc = s
+
+    salt = get_salt(sc)
+    z, k = ch % salt, len(s) - 1
+    while z < ch:
+        z *= z
+
+    while k > 0:
+        z %= salt
+        rmd = z
+        while k > 0 and rmd > k:
+            n = rmd % (k + 1)
+            s[k], s[n] = s[n], s[k]
+            k -= 1
+            rmd //= (k + 1)
+        if z == 0:
+            z = salt * salt + 1
+        else:
+            z *= z ^ (salt >> (salt.bit_length() - z.bit_length()))
+
+
+def tokenize(s, token=None):
+    if token is None:
+        token = list(token_bytes(key_len))
+
+    s, salt = list((8 + len(s)).to_bytes(8, 'big')) + s, get_salt(token)
+    noise(s, salt)
+
+    return token + s + [1] + list(token_bytes(2 * key_len + salt % int_sqrt(len(s) + key_len)))
+
+
+def unshuffle(sch):
+    sc = list(range(len(sch)))
+    shuffle(sc, sch)
+    s = [0] * len(sc)
+    for k in range(len(sc)):
+        s[sc[k]] = sch[k]
+
+    return s
+
+
+def untokenize(s):
+    token, s = s[:key_len], s[key_len:]
+    noise(s, get_salt(token))
+
+    return s[8:int.from_bytes(s[:8], 'big')]
+
+
+parser = ArgumentParser(description='Encription or decryption file by specifed cipher.')
+parser.add_argument('-v', '--verification', action='store_true', help='Print more information')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-e', "--encryption", action="store_true", help='Encryption file')
+group.add_argument('-d', "--decryption", action="store_true", help='Decryption file')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-dig', "--digital", action="store_true", help='Digital cipher')
+group.add_argument('-txt', "--text", action="store_true", help='Text cipher')
+group.add_argument('-bin', "--binary", action="store_true", help='Binary cipher')
+parser.add_argument('-c', '--cipher', metavar='file', type=str, required=True, help='Cipher file')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-b16', "--io_base16", action="store_true", help='base16 input or output file')
+group.add_argument('-b64', "--io_base64", action="store_true", help='base64 input or output file')
+group.add_argument('-b256', "--io_base256", action="store_true", help='Binary input or output file')
+parser.add_argument('-i', '--input', metavar='file', type=str, required=True, help='Input file')
+parser.add_argument('-o', '--output', metavar='file', type=str, required=True, help='Output file')
+
+args = parser.parse_args()
+
+## Common part
+
+if args.digital:
+    with open(args.cipher) as f:
+        ch = int(''.join(map(lambda s: s.strip(), f.readlines())), 0)
+elif args.text:
+    with open(args.cipher) as f:
+        ch = int.from_bytes(bytes(f.read(), 'utf-8'), 'big')
+elif args.binary:
+    with open(args.cipher, 'rb') as f:
+        ch = int.from_bytes(f.read(), 'big')
+
+if ch.bit_length() < 64:
+    exit('It is too short key!')
+
+if bits_few(ch):
+    exit('It is too weak key!')
+
+key_len = (ch.bit_length() + 7) // 8
+
+## Encription
+
+if args.encryption:
+    with open(args.input, 'rb') as f:
+        s = list(f.read())
+
+    s = tokenize(s)
+    s = list((int.from_bytes(s, 'big') // ch).to_bytes(len(s) - key_len + 1, 'big'))
+    noise(s)
+    shuffle(s)
+    s = bytes(s)
+    if args.io_base16 or args.io_base64:
+        if args.io_base16:
+            s = b16encode(s).decode()
+        elif args.io_base64:
+            s = b64encode(s).decode()
+
+        with open(args.output, 'w') as f:
+            f.writelines('\n'.join(s[i:i + 64] for i in range(0, len(s), 64)))
+
+    elif args.io_base256:
+        with open(args.output, 'wb') as f:
+            f.write(s)
+
+    print('Encryption completed!')
+
+## Decription
+
+elif args.decryption:
+    if args.io_base16 or args.io_base64:
+        with open(args.input) as f:
+            s = ''.join(map(lambda x: x.strip(), f.readlines()))
+
+        if args.io_base16:
+            s = b16decode(s)
+        elif args.io_base64:
+            s = b64decode(s)
+
+    elif args.io_base256:
+        with open(args.input, 'rb') as f:
+            s = list(f.read())
+
+    s = unshuffle(s)
+    noise(s)
+    s = list((int.from_bytes(s, 'big') * ch).to_bytes(len(s) + key_len - 1, 'big'))
+    s = untokenize(s)
+    with open(args.output, 'wb') as f:
+        f.write(bytes(s))
+
+    print('Decryption completed!')
